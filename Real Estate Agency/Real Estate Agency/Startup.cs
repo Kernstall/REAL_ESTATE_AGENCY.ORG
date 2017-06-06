30bb982000876b9b6619a862764190b9ebf1f9d4
Roman Buyak <evial@bk.ru>
2017-06-06 10:17:05 +0300

    Main project added

---
+++ Real Estate Agency/Real Estate Agency/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Real_Estate_Agency.Startup))]
+namespace Real_Estate_Agency
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
